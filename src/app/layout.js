d34454f6b3531ae3fe28202aa84e5b6745e3af36
CLCK0622 <zhongyi070622@163.com>
2025-01-21 20:39:10 +0800

    footer style

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,10 @@ import Footer from './components/footer';
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>{children}<Footer /></body>
+            <body className="app-container">
+                <main className="content">{children}</main>
+                <Footer />
+            </body>
         </html>
     );
 }
