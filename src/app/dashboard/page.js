cfd2adf0700eb5c4601ec2af5c604c045d319e2c
CLCK0622 <zhongyi070622@163.com>
2025-01-21 12:30:32 +0800

    dashboard frontend

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,10 +1,32 @@
 "use client";
 
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, Grid, Grid2 } from "@mui/material";
 import * as React from "react";
 import NavBar from "../nav-bar";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+
+function SchedulePlaceholder() {
+    return (
+        <Box
+            bgcolor="grey.200"
+            p={2}
+            textAlign="center"
+            borderRadius={2}
+            minHeight={300}
+        >
+            <Typography variant="h6" color="textSecondary">
+                Class Schedule (WIP)
+            </Typography>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateCalendar />
+            </LocalizationProvider>
+        </Box>
+    );
+}
 
 export default function Dashboard() {
     const router = useRouter();
@@ -39,9 +61,46 @@ export default function Dashboard() {
         <>
             <NavBar />
             <Container maxWidth="xl" sx={{ marginTop: 8 }}>
-                <Box><Typography sx={{ textAlign: "center", mt: 10, fontSize: 30 }}>Dashboard</Typography></Box>
-                <Box maxWidth="xl" bgcolor="white" sx={{ textAlign: "center", mt: 5 }}>Hi! {user ? (user.username) : "null user"}</Box>
-        </Container >
+                <Box>
+                    <Typography
+                        sx={{ textAlign: "center", mt: 10, fontSize: 30 }}
+                    >
+                        Dashboard
+                    </Typography>
+                </Box>
+                <Grid2 container spacing={3} sx={{ mt: 5 }}>
+                    <Grid2 item="true" size={6}>
+                        <Box
+                            bgcolor="white"
+                            p={2}
+                            borderRadius={2}
+                            boxShadow={1}
+                            textAlign="center"
+                        >
+                            <Typography variant="h6">
+                                Hi! {user ? user.username : "null user"}
+                            </Typography>
+                        </Box>
+                    </Grid2>
+                    <Grid2 item="true" size={6}>
+                        <Box
+                            bgcolor="white"
+                            p={2}
+                            borderRadius={2}
+                            boxShadow={1}
+                            textAlign="center"
+                        >
+                            <Typography variant="h6">Recent Classes</Typography>
+                            <Typography color="textSecondary">
+                                Classes Placeholder
+                            </Typography>
+                        </Box>
+                    </Grid2>
+                    <Grid2 item="true" size={12}>
+                        <SchedulePlaceholder />
+                    </Grid2>
+                </Grid2>
+            </Container >
         </>
     );
 }
